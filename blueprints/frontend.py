e0a2c18a067f94919a8ca4bc89aa071b100880ad
twentythreetea <helimc.minecraft@gmail.com>
2021-04-30 00:16:16 +0000

    fixed locations for new members

--- blueprints/frontend.py
+++ blueprints/frontend.py
@@ -460,8 +460,11 @@ async def register_post():
         safe_name = get_safe_name(username)
         
         # fetch the users' country
-        if request.headers and (ip := request.headers.get('X-Real-IP')):
-            country = await fetch_geoloc(ip)
+        if request.headers and (location := request.headers.get('CF-IPCountry')):
+            if location == "T1":
+                location = "XX"
+
+            country = location.lower()
         else:
             country = 'xx'
 
